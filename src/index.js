fe6a38292739f2bb3d16cc466e43ec658f387b6d
Keyan Zhang <kz5@indiana.edu>
2015-12-18 12:40:55 -0500

    more mocha

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-require('fetch-xhr'); // polyfill
+require('fetch-xhr'); // polyfill for loadin
 
-import React from 'react';
-import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 import 'normalize.css';
 import './styles/global.css';
+
+import React from 'react';
+import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createHistory } from 'history';
+// const history = createHistory();
+
+// if (__DEV__) {
+//   window.store = () => store.getState();
+// }
 
 import App from './App';
 
--- test/setup/domEnv.js
+++ test/setup/domEnv.js
@@ -9,4 +9,3 @@ export const unpatchDOMEnv = () => {
   global.document = null;
   global.window = null;
 };
-
