f37d419a031bec4ac29f9637e2d1da88960dabe5
Keyan Zhang <kz5@indiana.edu>
2016-01-17 16:21:22 -0500

    no more history

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import 'normalize.css';
 import './styles/global.css';
 
 import React from 'react';
+import { hashHistory } from 'react-router'
+
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createHistory } from 'history';
 import { syncReduxAndRouter } from 'redux-simple-router';
 
 import getRootRoute from './routes';
 import configStore from './store/configStore';
 
-const history = createHistory();
 // const store = configStore();
 
 // if (__DEV__) {
@@ -26,7 +26,7 @@ const history = createHistory();
 
 render(
 //  <Provider store={store}>
-    getRootRoute(history),
+    getRootRoute(hashHistory),
 //  </Provider>,
   document.getElementById('root'),
 );
